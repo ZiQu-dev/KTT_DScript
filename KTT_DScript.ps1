d867b3de7deb26bb082ae81e3cd0ac0e60d17cd7
Machux133 <machowskimateusz51@gmail.com>
2025-01-24 20:35:58 +0100

    zmiany w strukturze kodu

--- KTT_DScript.ps1
+++ KTT_DScript.ps1
@@ -1,5 +1,19 @@
+function Download-FileIfNotExists {
+    param (
+        [string]$url,
+        [string]$destinationPath
+    )
+
+    if (-Not (Test-Path -Path $destinationPath)) {
+        Invoke-WebRequest -Uri $url -OutFile $destinationPath
+        Write-Host "Pobrano: $destinationPath" -ForegroundColor Cyan
+    } else {
+        Write-Host "Plik $destinationPath juz istnieje. Pomijanie pobierania." -ForegroundColor Yellow
+    }
+}
+
 Write-Host "Wybierz jedna z ponizszych opcji:" -ForegroundColor Cyan
-Write-Host "1: Jestem uzytkownikiem Minecraft Premium i chce uzyc oficjalnego launchera"
+Write-Host "1: Jestem uzytkownikiem Minecraft Premium i chce uzyc oficjalnego launchera (UZYWAC TYLKO W OSTATECZNOSCI)" -ForegroundColor Red -BackgroundColor Gray
 Write-Host "2: Jestem uzytkownikiem Minecraft Premium i chce uzyc Prism Launcher (rekomendowane)"
 Write-Host "3: Jestem uzytkownikiem Minecraft Non-Premium i chce uzyc Fjord Launcher"
 Write-Host "4: Przywroc konfiguracje Minecraft sprzed instalacji (Wylacznie do uzytku po uzyciu opcji 1)"
@@ -14,26 +28,10 @@ switch ($choice) {
         $file1 = Join-Path $scriptPath "KTT MiniGry Alpha.mrpack"
         $file2 = Join-Path $scriptPath "mrpack-downloader-win.exe"
         $fabricInstaller = Join-Path $scriptPath "fabric-installer-1.0.1.exe"
-        if (-Not (Test-Path -Path $file1)) {
-            Invoke-WebRequest -Uri "https://dobraszajba.com:8000/KTT%20MiniGry%20Alpha.mrpack" -OutFile $file1
-            Write-Host "Pobrano: KTT MiniGry Alpha.mrpack" -ForegroundColor Cyan
-        } else {
-            Write-Host "Plik KTT MiniGry Alpha.mrpack juz istnieje. Pomijanie pobierania." -ForegroundColor Yellow
-        }
 
-        if (-Not (Test-Path -Path $file2)) {
-            Invoke-WebRequest -Uri "https://dobraszajba.com:8000/mrpack-downloader-win.exe" -OutFile $file2
-            Write-Host "Pobrano: mrpack-downloader-win.exe" -ForegroundColor Cyan
-        } else {
-            Write-Host "Plik mrpack-downloader-win.exe juz istnieje. Pomijanie pobierania." -ForegroundColor Yellow
-        }
-
-        if (-Not (Test-Path -Path $fabricInstaller)) {
-            Invoke-WebRequest -Uri "https://dobraszajba.com:8000/fabric-installer-1.0.1.exe" -OutFile $fabricInstaller
-            Write-Host "Pobrano: fabric-installer-1.0.1.exe" -ForegroundColor Cyan
-        } else {
-            Write-Host "Plik fabric-installer-1.0.1.exe juz istnieje. Pomijanie pobierania." -ForegroundColor Yellow
-        }
+        Download-FileIfNotExists -url "https://dobraszajba.com:8000/KTT%20MiniGry%20Alpha.mrpack" -destinationPath $file1
+        Download-FileIfNotExists -url "https://dobraszajba.com:8000/mrpack-downloader-win.exe" -destinationPath $file2
+        Download-FileIfNotExists -url "https://dobraszajba.com:8000/fabric-installer-1.0.1.exe" -destinationPath $fabricInstaller
 
         # Instalacja Fabric
         $userProfile = [Environment]::GetFolderPath("UserProfile")
@@ -55,38 +53,42 @@ switch ($choice) {
         }
     }
     "2" {
-    Write-Host "Wybrano: Prism Launcher (rekomendowane dla uzytkownikow Minecraft Premium)." -ForegroundColor Green
-    
-    # Okreslenie sciezki do pliku konfiguracyjnego
-    $userProfile = [Environment]::GetFolderPath("UserProfile")
-    $prismLauncherDir = Join-Path $userProfile "AppData\Roaming\PrismLauncher"
-    $configFile = Join-Path $prismLauncherDir "prismlauncher.cfg"
+        Write-Host "Wybrano: Prism Launcher (rekomendowane dla uzytkownikow Minecraft Premium)." -ForegroundColor Green
+        
+        # Okreslenie sciezki do pliku konfiguracyjnego
+        $userProfile = [Environment]::GetFolderPath("UserProfile")
+        $prismLauncherDir = Join-Path $userProfile "AppData\Roaming\PrismLauncher"
+        $configFile = Join-Path $prismLauncherDir "prismlauncher.cfg"
 
-    # Sprawdzenie, czy plik istnieje i zawiera dane
-    if (Test-Path -Path $configFile) {
-        $fileContent = Get-Content -Path $configFile -ErrorAction Stop
-        if ($fileContent.Trim() -ne "") {
-            Write-Host "Plik 'prismlauncher.cfg' znaleziony i zawiera dane. Pomijanie instalacji." -ForegroundColor Green
-            return
+        # Sprawdzenie, czy plik istnieje i zawiera dane
+        if (Test-Path -Path $configFile) {
+            $fileContent = Get-Content -Path $configFile -ErrorAction Stop
+            if ($fileContent.Trim() -ne "") {
+                Write-Host "Plik 'prismlauncher.cfg' znaleziony i zawiera dane. Pomijanie instalacji." -ForegroundColor Green
+                return
+            }
         }
-    }
-    
-    Write-Host "Plik 'prismlauncher.cfg' nie istnieje lub jest pusty. Pobieranie instalatora Prism Launcher..." -ForegroundColor Yellow
-    
-    # Ścieżka do zapisu instalatora
-    $tempPath = [System.IO.Path]::GetTempPath()
-    $installerPath = Join-Path $tempPath "PrismLauncher-Setup.exe"
-    $downloadUrl = "https://github.com/PrismLauncher/PrismLauncher/releases/download/9.2/PrismLauncher-Windows-MSVC-Setup-9.2.exe"
+        
+        Write-Host "Plik 'prismlauncher.cfg' nie istnieje lub jest pusty. Pobieranie instalatora Prism Launcher..." -ForegroundColor Yellow
+        
+        # Ścieżka do zapisu instalatora
+        $tempPath = [System.IO.Path]::GetTempPath()
+        $installerPath = Join-Path $tempPath "PrismLauncher-Setup.exe"
+        $downloadUrl = "https://github.com/PrismLauncher/PrismLauncher/releases/download/9.2/PrismLauncher-Windows-MSVC-Setup-9.2.exe"
 
-    # Pobranie instalatora
-    Invoke-WebRequest -Uri $downloadUrl -OutFile $installerPath
-    Write-Host "Pobrano instalator do: $installerPath" -ForegroundColor Cyan
+        # Pobranie instalatora
+        Download-FileIfNotExists -url $downloadUrl -destinationPath $installerPath
 
-    # Uruchamianie instalatora
-    Start-Process -FilePath $installerPath -Wait
-    Write-Host "Uruchomiono instalator Prism Launcher. Kontynuuj instalację zgodnie z instrukcjami." -ForegroundColor Green
+        # Uruchamianie instalatora
+        Start-Process -FilePath $installerPath -Wait
+        Write-Host "Uruchomiono instalator Prism Launcher. Kontynuuj instalację zgodnie z instrukcjami." -ForegroundColor Green
 
-    #TODO: Dodaj dalsze kroki konfiguracji po instalacji, jeśli wymagane
+        $prismInstallation = Read-Host "czy instalacja zostala zakonczona pomyślnie? (y/n)" -ForegroundColor Yellow
+        if ($prismInstallation -eq "y") {
+            Write-Host "Instalacja Prism Launcher zostala zakonczona." -ForegroundColor Green
+        } else {
+            Write-Host "Sprobuj ponowic instalacje recznie a nastepnie uruchom ponownie skrypt" -ForegroundColor Red
+        }
     }
 
     "3" {
